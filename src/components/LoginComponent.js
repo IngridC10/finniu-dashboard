b4e3952fc4e2b7c34741a32e2f9d4040c1443a30
Ingrid <ingridcalzada@MacBook-Pro-de-Ingrid-2.local>
2024-01-06 23:06:09 -0500

    se esta haciendo el login

--- src/components/LoginComponent.js
+++ src/components/LoginComponent.js
@@ -1,104 +1,79 @@
-import React, { useState } from "react";
-import { useMutation, gql } from "@apollo/client";
 import LogoFinniu from "../images/LogoFinniu.png";
 import styled from "styled-components";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEye, faSquare } from "@fortawesome/free-solid-svg-icons";
+import React, { useState } from "react";
+import { useMutation, gql } from "@apollo/client";
 
-const LogoImage = styled.img`
-  width: 170px;
-  height: 70px;
-`;
-const LoginContainer = styled.div`
-  padding: 20px;
-  min-height: 100vh; /* Cambiado a min-height para asegurar el llenado del viewport */
-  box-sizing: border-box;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background: linear-gradient(rgba(219, 247, 255, 1), rgba(255, 238, 221, 0));
-`;
-
-const LoginForm = styled.form`
-  width: 90%;
-  max-width: 400px;
-  border-radius: 20px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 20px;
-  background: white;
-
-  @media screen and (max-width: 768px) {
-    /* Cambios para pantallas más pequeñas */
-    width: 80%;
+const LoginComponentStyled = styled.div`
+  &.bodyLogin {
+    width: 100%;
+    height: 100%;
+    background: linear-gradient(rgba(219, 247, 255, 1), rgba(255, 238, 221, 0));
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
   }
-`;
-
-const Title = styled.h1`
-  font-size: 2em; /* Cambiado a unidades em para adaptarse mejor */
-  font-weight: normal;
-  width: 80%;
-  margin-bottom: 3px;
-  padding-bottom: 20px;
-  text-align: center;
-
-  @media screen and (max-width: 768px) {
-    font-size: 1.8em;
+  .container {
+    margin: auto;
+    width: 835px;
+    height: 759px;
+    background: white;
+    border-radius: 50px;
   }
-`;
 
-const LoginInput = styled.input`
-  padding: 10px;
-  margin-bottom: 20px;
-  width: 80%;
-  max-width: 300px;
-  height: 23px;
-  border-radius: 26px;
-  border: 1.9px solid rgba(162, 230, 250, 1);
-`;
+  .login {
+    display: flex;
+    flex-direction: column;
+  }
 
-const ForgotPassword = styled.p`
-  margin-left: auto;
-  margin-right: 45px;
-  max-width: 80%;
-  font-size: 10px;
-  margin-top: -10px;
-  text-align: right;
-`;
+  .login img {
+    width: 261px;
+    height: 127px;
+    align-self: center;
+    margin-button: 15px;
+  }
 
-const NoAccount = styled.p`
-  font-size: 13px;
-  margin-top: 8px;
-`;
+  .login h1 {
+    font-size: 32px;
+    font-weight: normal;
+  }
 
-const Save = styled.div`
-  display: flex;
-  align-items: center;
-  font-size: 10px;
-  margin-top: -10px;
-  gap: 9px;
-`;
+  .login input {
+    margin: 15px;
+    align-self: center;
+    width: 498px;
+    height: 45px;
+    border-radius: 26px;
+    border: 1.9px solid rgba(162, 230, 250, 1);
+  }
+  .login h2 {
+    font-size: 16px;
+    font-weight: normal;
+    display: flex;
+    justify-content: flex-end;
+  }
+  .login h3 {
+    font-size: 24px;
+    font-weight: normal;
+  }
+  .login button {
+    width: 184px;
+    height: 48px;
+    color: white;
+    border-radius: 20px;
+    background-color: rgba(13, 58, 92, 1);
+    font-weight: bold;
+    font-size: 18px;
+    align-self: center;
+  }
 
-const EyeIcon = styled.span`
-  position: absolute;
-  right: 60px;
-  top: 26px;
-  transform: translateY(-50%);
-  cursor: pointer;
-`;
-
-const LoginButton = styled.button`
-  width: 100px;
-  height: 30px;
-  color: white;
-  border-radius: 20px;
-  background-color: rgba(13, 58, 92, 1);
-  font-weight: bold;
-`;
-
-const StyledFontAwesomeIcon = styled(FontAwesomeIcon)`
-  width: 25px;
+  .savePasswordSection {
+    font-size: 16px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+  }
 `;
 
 const LOGIN_USER = gql`
@@ -132,45 +107,126 @@ const LoginComponent = () => {
     }
   };
 
-  const togglePasswordVisibility = () => {
-    setShowPassword(!showPassword);
-  };
-
   return (
-    <LoginContainer>
-      <LoginForm>
-        <LogoImage src={LogoFinniu} alt="Imagen de login" />
-        <Title>Hola, ingresa a tu cuenta</Title>
-        <LoginInput
-          type="text"
-          placeholder="Correo electrónico"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <div style={{ position: "center", width: "100%" }}>
-          <LoginInput
+    <LoginComponentStyled className="bodyLogin">
+      <div className="container">
+        <div className="login">
+          <img src={LogoFinniu} alt="logo" />
+          <h1>Hola, ingresa a tu cuenta</h1>
+          <input
+            type="text"
+            placeholder="Correo electrónico"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
             type={showPassword ? "text" : "password"}
             placeholder="Contraseña"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
-          <EyeIcon onClick={togglePasswordVisibility}>
-            <FontAwesomeIcon icon={faEye} />
-          </EyeIcon>
-        </div>
-        <ForgotPassword>¿Olvidaste tu contraseña?</ForgotPassword>
-        <Save>
-          <FontAwesomeIcon icon={faSquare} />
-          <p>Guardar mis credenciales para los futuros ingresos</p>
-        </Save>
 
-        <LoginButton type="button" onClick={handleLogin}>
-          Entrar
-        </LoginButton>
-        <NoAccount>¿Aún no tienes una cuenta?</NoAccount>
-      </LoginForm>
-    </LoginContainer>
+          <h2>¿Olvidaste tu contraseña?</h2>
+          <div className="savePasswordSection">
+            <h2>Cuadrado</h2>
+            <h2>Guardar mis credenciales para los futuros ingresos</h2>
+          </div>
+          <button onClick={handleLogin}>Entrar</button>
+          <h3>¿Aún no tienes una cuenta?</h3>
+        </div>
+      </div>
+    </LoginComponentStyled>
   );
 };
 
 export default LoginComponent;
+
+// import React, { useState } from "react";
+// import { useMutation, gql } from "@apollo/client";
+// import LogoFinniu from "../images/LogoFinniu.png";
+// import styled from "styled-components";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faEye, faSquare } from "@fortawesome/free-solid-svg-icons";
+// const AppContainer = styled.div`
+//   background: linear-gradient(rgba(219, 247, 255, 1), rgba(255, 238, 221, 0));
+// `;
+
+// const LOGIN_USER = gql`
+//   mutation TokenAuth($email: String!, $password: String!) {
+//     tokenAuth(email: $email, password: $password) {
+//       token
+//     }
+//   }
+// `;
+
+// const LoginComponent = () => {
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+//   const [showPassword, setShowPassword] = useState(false);
+
+//   const [loginUser] = useMutation(LOGIN_USER);
+
+//   const handleLogin = async () => {
+//     try {
+//       const { data: loginResponse } = await loginUser({
+//         variables: { email, password },
+//       });
+
+//       if (loginResponse?.tokenAuth?.token) {
+//         console.log("Inicio de sesión exitoso. Redirigiendo...");
+//       } else {
+//         console.log("Credenciales incorrectas o usuario no encontrado");
+//       }
+//     } catch (error) {
+//       console.error("Error al iniciar sesión:", error);
+//     }
+//   };
+
+//   const togglePasswordVisibility = () => {
+//     setShowPassword(!showPassword);
+//   };
+
+//   return (
+//     <AppContainer>
+//       <LoginContainer>
+//         <LoginForm>
+//           <LogoImage src={LogoFinniu} alt="Imagen de login" />
+//           <Title>Hola, ingresa a tu cuenta</Title>
+//           <LoginInput
+//             type="text"
+//             placeholder="Correo electrónico"
+//             value={email}
+//             onChange={(e) => setEmail(e.target.value)}
+//           />
+//           <div style={{ position: "center", width: "100%" }}>
+//             <LoginInput
+//               type={showPassword ? "text" : "password"}
+//               placeholder="Contraseña"
+//               value={password}
+//               onChange={(e) => setPassword(e.target.value)}
+//             />
+//             <EyeIcon onClick={togglePasswordVisibility}>
+//               <FontAwesomeIcon icon={faEye} />
+//             </EyeIcon>
+//           </div>
+//           <div>
+//             <ForgotPassword>¿Olvidaste tu contraseña?</ForgotPassword>
+//           </div>
+//           <Save>
+//             <div>
+//               {" "}
+//               <FontAwesomeIcon icon={faSquare} />{" "}
+//               <p>Guardar mis credenciales para los futuros ingresos</p>
+//             </div>
+//           </Save>
+
+//           <LoginButton type="button" onClick={handleLogin}>
+//             Entrar
+//           </LoginButton>
+//           <NoAccount>¿Aún no tienes una cuenta?</NoAccount>
+//         </LoginForm>
+//       </LoginContainer>
+//     </AppContainer>
+//   );
+// };
+// export default LoginComponent;
