a7165e20658fe8839d54f3ae9b61b5e3a3d51413
Ingrid <ingridcalzada@MacBook-Pro-de-Ingrid-2.local>
2024-01-07 01:48:38 -0500

    se hizo el ojito del login

--- src/components/LoginComponent.js
+++ src/components/LoginComponent.js
@@ -2,6 +2,9 @@ import LogoFinniu from "../images/LogoFinniu.png";
 import styled from "styled-components";
 import React, { useState } from "react";
 import { useMutation, gql } from "@apollo/client";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSquare } from "@fortawesome/free-solid-svg-icons";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 
 const LoginComponentStyled = styled.div`
   &.bodyLogin {
@@ -19,6 +22,10 @@ const LoginComponentStyled = styled.div`
     height: 759px;
     background: white;
     border-radius: 50px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
   }
 
   .login {
@@ -41,6 +48,7 @@ const LoginComponentStyled = styled.div`
   .login input {
     margin: 15px;
     align-self: center;
+    padding-left: 20px;
     width: 498px;
     height: 45px;
     border-radius: 26px;
@@ -51,6 +59,7 @@ const LoginComponentStyled = styled.div`
     font-weight: normal;
     display: flex;
     justify-content: flex-end;
+    margin-right: 25px;
   }
   .login h3 {
     font-size: 24px;
@@ -67,7 +76,15 @@ const LoginComponentStyled = styled.div`
     align-self: center;
   }
 
+  .password {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+  }
+
   .savePasswordSection {
+    gap: 2%;
     font-size: 16px;
     display: flex;
     flex-direction: row;
@@ -106,6 +123,9 @@ const LoginComponent = () => {
       console.error("Error al iniciar sesión:", error);
     }
   };
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
 
   return (
     <LoginComponentStyled className="bodyLogin">
@@ -119,17 +139,32 @@ const LoginComponent = () => {
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
-          <input
-            type={showPassword ? "text" : "password"}
-            placeholder="Contraseña"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
+          <div className="password">
+            <input
+              type={showPassword ? "text" : "password"}
+              placeholder="Contraseña"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <div className="icon">
+              <FontAwesomeIcon
+                style={{
+                  float: "left",
+                  marginLeft: "-78px",
+                  marginBottom: "-4px",
+                  width: "25px",
+                  height: "30px",
+                }}
+                icon={showPassword ? faEyeSlash : faEye}
+                onClick={togglePasswordVisibility}
+              />
+            </div>
+          </div>
 
           <h2>¿Olvidaste tu contraseña?</h2>
           <div className="savePasswordSection">
-            <h2>Cuadrado</h2>
-            <h2>Guardar mis credenciales para los futuros ingresos</h2>
+            <FontAwesomeIcon icon={faSquare} style={{ background: "white" }} />{" "}
+            <p>Guardar mis credenciales para los futuros ingresos</p>
           </div>
           <button onClick={handleLogin}>Entrar</button>
           <h3>¿Aún no tienes una cuenta?</h3>
@@ -140,93 +175,3 @@ const LoginComponent = () => {
 };
 
 export default LoginComponent;
-
-// import React, { useState } from "react";
-// import { useMutation, gql } from "@apollo/client";
-// import LogoFinniu from "../images/LogoFinniu.png";
-// import styled from "styled-components";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faEye, faSquare } from "@fortawesome/free-solid-svg-icons";
-// const AppContainer = styled.div`
-//   background: linear-gradient(rgba(219, 247, 255, 1), rgba(255, 238, 221, 0));
-// `;
-
-// const LOGIN_USER = gql`
-//   mutation TokenAuth($email: String!, $password: String!) {
-//     tokenAuth(email: $email, password: $password) {
-//       token
-//     }
-//   }
-// `;
-
-// const LoginComponent = () => {
-//   const [email, setEmail] = useState("");
-//   const [password, setPassword] = useState("");
-//   const [showPassword, setShowPassword] = useState(false);
-
-//   const [loginUser] = useMutation(LOGIN_USER);
-
-//   const handleLogin = async () => {
-//     try {
-//       const { data: loginResponse } = await loginUser({
-//         variables: { email, password },
-//       });
-
-//       if (loginResponse?.tokenAuth?.token) {
-//         console.log("Inicio de sesión exitoso. Redirigiendo...");
-//       } else {
-//         console.log("Credenciales incorrectas o usuario no encontrado");
-//       }
-//     } catch (error) {
-//       console.error("Error al iniciar sesión:", error);
-//     }
-//   };
-
-//   const togglePasswordVisibility = () => {
-//     setShowPassword(!showPassword);
-//   };
-
-//   return (
-//     <AppContainer>
-//       <LoginContainer>
-//         <LoginForm>
-//           <LogoImage src={LogoFinniu} alt="Imagen de login" />
-//           <Title>Hola, ingresa a tu cuenta</Title>
-//           <LoginInput
-//             type="text"
-//             placeholder="Correo electrónico"
-//             value={email}
-//             onChange={(e) => setEmail(e.target.value)}
-//           />
-//           <div style={{ position: "center", width: "100%" }}>
-//             <LoginInput
-//               type={showPassword ? "text" : "password"}
-//               placeholder="Contraseña"
-//               value={password}
-//               onChange={(e) => setPassword(e.target.value)}
-//             />
-//             <EyeIcon onClick={togglePasswordVisibility}>
-//               <FontAwesomeIcon icon={faEye} />
-//             </EyeIcon>
-//           </div>
-//           <div>
-//             <ForgotPassword>¿Olvidaste tu contraseña?</ForgotPassword>
-//           </div>
-//           <Save>
-//             <div>
-//               {" "}
-//               <FontAwesomeIcon icon={faSquare} />{" "}
-//               <p>Guardar mis credenciales para los futuros ingresos</p>
-//             </div>
-//           </Save>
-
-//           <LoginButton type="button" onClick={handleLogin}>
-//             Entrar
-//           </LoginButton>
-//           <NoAccount>¿Aún no tienes una cuenta?</NoAccount>
-//         </LoginForm>
-//       </LoginContainer>
-//     </AppContainer>
-//   );
-// };
-// export default LoginComponent;
