30864c6b4c934624a0ff580e362b08868ab2b594
Ingrid <ingridcalzada@MacBook-Pro-de-Ingrid-2.local>
2024-01-07 22:46:13 -0500

    se mejoro el app

--- src/components/InprogressInvestmentReport.js
+++ src/components/InprogressInvestmentReport.js
@@ -2,62 +2,79 @@ import React from "react";
 import styled from "styled-components";
 
 const InprogressInvestmentReportStyled = styled.div`
-  .ContainerPadre {
-    margin: 30px;
+  .investmentReport {
+    margin: 20px;
     border-radius: 30px;
     background-color: rgba(219, 247, 255, 1);
-    width: 510px;
-    height: 360px;
     padding: 30px;
-    display: grid;
-    grid-template-columns: 1fr;
-    grid-template-rows: 1fr 1fr 1fr;
-    align-items: center;
-    justify-content: center;
   }
-  .ContainerHijo1 {
-    display: flex;
-    justify-content: space-between;
+  .bodyReport {
+    width: 598px;
+    height: 263px;
     align-items: center;
-    padding-right: 45px;
-    padding-left: 45px;
     color: rgba(13, 58, 92, 1);
     font-weight: bold;
     font-size: 24px;
-  }
-  .ContainerHijo2 {
-    font-size: 30px;
     display: grid;
-    grid-template-columns: 1fr 1fr;
-    grid-template-rows: 1fr 1fr 1fr;
+    grid-template-columns: 1fr 1fr 1fr;
   }
-
-  .ContainerHijo3 {
-    display: grid;
-    grid-template-rows: 1fr;
+  .bodyReportColumn1 {
+    font-size: 36px;
+    font-weight: bold;
+  }
+  .bodyReportRow {
+    text-align: left;
+    font-size: 18px;
   }
 `;
 
 const InprogressInvestmentReport = () => {
   return (
     <InprogressInvestmentReportStyled>
-      <div className="ContainerPadre">
-        <div className="ContainerHijo1">
-          <div>Mi inversión en curso</div>
-          <div style={{ fontSize: "15px", fontWeight: "normal" }}>Soles</div>
+      <div className="investmentReport">
+        <div className="bodyReportRow">
+          <h1>Mi inversión en curso</h1>
         </div>
 
-        <div className="ContainerHijo2">
-          <div>S/3318.5</div>
-          <div style={{ fontSize: "16px" }}>Total de intereses generados</div>
-          <div style={{ fontSize: "16px" }}>Dinero inicial</div>
+        <div className="bodyReport">
+          <div className="bodyReportColumn1">
+            <p>s/3318.5</p>
+            <div
+              style={{
+                fontSize: "16px",
+                padding: "10px",
+                fontWeight: "normal",
+              }}
+            >
+              <p>Dinero inicial</p>
+            </div>
+            <div style={{ fontWeight: "normal" }}>s/3000</div>
+          </div>
 
-          <div style={{ color: "rgba(50, 160, 113, 1)" }}>S/318</div>
-          <div>S/3000</div>
-        </div>
+          <div className="bodyReportColumn2">
+            <p>linea</p>
+          </div>
 
-        <div className="ContainerHijo3">
-          <div>Ver todas mis inversiones</div>
+          <div className="bodyReportColumn3">
+            <div
+              style={{
+                fontSize: "16px",
+                paddingTop: "10px",
+                fontWeight: "normal",
+              }}
+            >
+              <p>Total de intereses generados</p>
+            </div>
+            <div
+              style={{
+                color: "rgba(50, 160, 113, 1)",
+                fontWeight: "normal",
+                fontSize: "36px",
+              }}
+            >
+              <p>s/318.5</p>
+            </div>
+          </div>
         </div>
       </div>
     </InprogressInvestmentReportStyled>
